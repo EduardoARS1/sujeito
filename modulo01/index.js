2e712c07605c330c5613f8b539d1b5cab3389480
EduardoARS1 <eduardoalvarenga.jp@hotmail.com>
2025-06-19 22:43:51 -0300

    :sparkles: Aula sobre middlewares concluída

--- modulo01/index.js
+++ modulo01/index.js
@@ -6,6 +6,47 @@ const server = express();
 
 server.use(express.json());
 
+//Middlewares
+
+//Exibe as rotas quando acessadas
+server.use((req, res, next)=> {
+    console.log(`ROTA ACESSADA: ${req.url}`)
+
+    return next();
+});
+
+//função de middleware para verificar se a propriedade é passada, se não retorna erro
+function checkBody (req, res, next) {
+
+    if(!req.body.name) {
+        return res.status(400).json({ message: "Propriedade 'name' é obrigatória"})
+    };
+
+    return next();
+};
+
+function checkIndexCurso(req, res, next) {
+    const curso = cursos[req.params.index];
+
+    if(!curso){
+        return res.status(400).json({ message: "Esse curso não existe"})
+    };
+
+    req.curso = curso;
+
+    return next();
+};
+
+// Tentei criar um middleware para uma rota onde o ID é obrigatório, mas não faz sentido, pois se não passar o ID a consulta já quebra a não ser que eu criasse uma outra rota, sem o ID e nela retornasse um erro, mas não faria sentido
+// function checkPowerId(req, res, next){
+
+//     if(!req.params.id){
+//         return res.status(400).json({ message: "Esse curso na rota power não existe"})
+//     };
+
+//     return next();
+// };
+
 server.get('/videos', (req,res) => {
   
     // return res.send('Hello World!'); usado para teste e retornar no navegador o texto
@@ -37,11 +78,12 @@ const cursos = ['Node JS', 'JavaScript', 'PHP', 'TypeScript', 'React Native'];
 //CRUD
 
 //Listando um curso
-server.get('/curso/:index', (req, res) => {
+server.get('/curso/:index', checkIndexCurso,(req, res) => {
     
-    const { index } = req.params;
-
-    return res.json({curso: cursos[index]});
+    // const { index } = req.params;
+    // return res.json({curso: cursos[index]});
+    return res.json({curso: req.curso});
+    
 });
 
 //Listando todos os cursos
@@ -52,7 +94,7 @@ server.get('/cursos', (req, res) => {
 });
 
 //Criando um curso
-server.post('/cursos', (req, res) => {
+server.post('/cursos', checkBody, (req, res) => {
 
     const { name } = req.body; //define o parametro no body para receber o valor
 
@@ -63,7 +105,7 @@ server.post('/cursos', (req, res) => {
 });
 
 //Atualizando um curso
-server.put('/curso/:index', (req, res) => {
+server.put('/curso/:index', checkBody, checkIndexCurso,(req, res) => {
 
     const { index } = req.params;
 
@@ -76,7 +118,7 @@ server.put('/curso/:index', (req, res) => {
 });
 
 //Deletando um curso
-server.delete('/curso/:index', (req, res) =>{
+server.delete('/curso/:index', checkIndexCurso,(req, res) =>{
 
     const { index } = req.params;
 
