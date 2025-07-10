cfa3506e8aed5a3282eb558e7f627e52e893bac9
EduardoARS1 <eduardoalvarenga.jp@hotmail.com>
2025-07-09 23:18:07 -0300

    :sparkles: Iniciando os estudos com JavaScript

---
+++ JavaScript/script-aula01.js
@@ -0,0 +1,8 @@
+
+var nome = prompt("Qual o seu nome?");
+
+var sobrenome = prompt("Qual o seu sobrenome?");
+
+document.writeln("<h1>Bem vindo " + nome + " " + sobrenome + "!</h1>");
+
+document.writeln("<img src='./imagens/perfil 3x4.jpg' width='300' height='550' alt='Foto perfil'/>");
